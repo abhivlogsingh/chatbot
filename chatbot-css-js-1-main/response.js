6fd820586d34052e77df4431708bdbd022899dde
Abhishek Singh <73503674+abhivlogsingh@users.noreply.github.com>
2023-05-29 22:28:29 +0530

    Add files via upload

---
+++ chatbot-css-js-1-main/response.js
@@ -0,0 +1,14 @@
+const responseObj = {
+  hello: "Hey ! How are you doing ?",
+  hey: "Hey! What's Up",
+  who : "I am Rama ChatBot",
+  admission: "https://collegedunia.com/university/56477-rama-university-kanpur",
+  iamstudent: "hii !! student",
+  dirty: "no this side rama chatbot",
+  iamasksomequestions: "yes tell me please",
+  
+  contact: "113467854",
+  whatisyourname: "hii ! I am Rama ChatBot",
+  today: new Date().toDateString(),
+  time: new Date().toLocaleTimeString(),
+};
